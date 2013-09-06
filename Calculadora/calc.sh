6c5bfdf015ae1526280cdde77e471df079066afb
arevaloarboled <arevaloarboled@gmail.com>
2013-09-05 20:46:54 -0500

    Script Final

--- Calculadora/calc.sh
+++ Calculadora/calc.sh
@@ -19,3 +19,21 @@ function resta() {
 	echo $(echo "$1 - $2" | bc)
 }
 
+case "$2" in 
+"+") 
+sumar  $1 $3
+;;
+"-")
+resta $1 $3
+;;
+"x")
+multiplicacion $1 $3
+;;
+"/")
+division $1 $3
+;;
+*)
+echo "Usage: $0 num1 {+|-|x|/} num2"
+esac
+
+
