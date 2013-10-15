e005e6e7e22f4f1a36c5e4e841b886c7697fbf4c
Sirquini <sirquini2@gmail.com>
2013-10-14 19:36:57 -0500

    Agregada implementacion inicial de Zenity

--- metricas-py/script.sh
+++ metricas-py/script.sh
@@ -3,25 +3,27 @@ TLINEAS_OUT="Total.txt"
 COMMS_OUT="Comments.txt"
 BLANKS_OUT="Blancas.txt"
 
-if [-f $FUNCIONES_OUT]
+if [ -f $FUNCIONES_OUT ]
 then
     rm $FUNCIONES_OUT
 fi
 
-if [-f $TLINEAS_OUT]
+if [ -f $TLINEAS_OUT ]
 then
     rm $TLINEAS_OUT
 fi
 
-if [-f $COMMS_OUT]
+if [ -f $COMMS_OUT ]
 then
     rm $COMMS_OUT
 fi
 
-if [-f $BLANKS_OUT]
+if [ -f $BLANKS_OUT ]
 then
     rm $BLANKS_OUT
 fi
+
+SCRIPTS_IN=`zenity --file-selection --multiple --separator=" "`
 
 # 
 # Total de lineas de codigo 
@@ -49,12 +51,21 @@ function funciones()
         echo $1 $(grep -c "def " $1) >> $FUNCIONES_OUT
 }
 
+#
+# Total de lineas blancas
+#
+
 function lines()
 {
-  echo $(grep -ce ^$ $1) >> $BLANKS_OUT
+  echo $1 $(grep -ce ^$ $1) >> $BLANKS_OUT
 }
 
-# lines $1
-# t_lineas $1
-# comment $1
-# funciones $1
+for SCRIPT_IN in ${SCRIPTS_IN[@]}
+do
+	lines $SCRIPT_IN
+	t_lineas $SCRIPT_IN
+	comment $SCRIPT_IN
+	funciones $SCRIPT_IN
+done
+
+exit 0
